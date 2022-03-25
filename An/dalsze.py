585dbbf4ff54bc89c0329e62c736e6123b15a1f4
Micu <102016311+Micumon@users.noreply.github.com>
2022-03-25 14:38:13 +0100

    Delete An directory

--- An/dalsze.py
+++
@@ -1,8 +0,0 @@
-import os
-
-path = "Z:\$$01_Winkalk\@Winkalk 2022\\009_2022 Moskwa Projektówka\Operat wznowienie\Doręczenia\dorki"
-os.chdir(path)
-lista = os.listdir(path)
-print(lista)
-for i in range(len(lista)):
-    os.rename(lista[i],str(i)+".pdf")
