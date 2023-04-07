c1f87eb58dd29b4c65335ae0360063abdf1cc9ea
Micu <micking3@gmail.com>
2023-04-07 15:34:59 +0200

    push

--- Lekcja Klasy/main.py
+++ Lekcja Klasy/main.py
@@ -1,8 +1,12 @@
+import pickle
+import glob
+
 class Cake:
 
-    known_types = ['cake', 'muffin', 'meringue', 'biscuit', 'eclair', 'christmas', 'pretzel','other']
+    known_types = ['cake', 'muffin', 'meringue', 'biscuit', 'eclair', 'christmas', 'pretzel', 'other']
     bakery_offer = []
-    def __init__(self, cake_name, cake_kind, cake_taste, cake_additives, cake_filling):
+
+    def __init__(self, cake_name, cake_kind, cake_taste, cake_additives, cake_filling, gluten, text):
         self.name = cake_name
         if cake_kind in Cake.known_types:
             self.kind = cake_kind
@@ -11,15 +15,33 @@ class Cake:
         self.taste = cake_taste
         self.additives = list(cake_additives).copy()
         self.filling = cake_filling
+        self.__gluten_free = gluten
+        if cake_kind == "cake":
+            self.__text = text
+        else:
+            self.__text = ""
         Cake.bakery_offer.append(self)
 
+    def __get_text(self):
+        return self.__text
+
+    def __set_text(self, new_text):
+        if self.kind == "cake":
+            self.__text = new_text
+        else:
+            print(f"{self.name} is not cake!")
+    Text = property(__get_text, __set_text)
     def show_info(self):
         print(self.name.upper())
+        print(f"Type: {self.kind}")
         print(f"Taste: {self.taste}")
         if self.additives:
             print("Additives: " + ", ".join(self.additives))
         if self.filling != "":
             print(f"Filling: {self.filling}")
+        print(f"Gluten free: {self.__gluten_free}")
+        if self.kind == "cake":
+            print(f"Text on cake: \"{self.Text}\"")
         print("***************************")
 
     def set_filling(self, new_filling):
@@ -29,12 +51,32 @@ class Cake:
         for add in new_additives:
             self.additives.append(add)
 
+    def save_to_file(self, path):
+        with open(path,"wb") as file:
+            pickle.dump(self, file)
 
-vanila_cake = Cake("Vanila Cake","cake","vanilla", ["chocolade","nuts"], "cream")
-chocolade_muffin = Cake("Chocolade Muffin", "muffin","chocolade", "", "")
-cake04 = Cake('Cocoa waffle','waffle','cocoa',[],'cocoa')
+    @classmethod
+    def read_from_file(cls, path):
+        with open(path, "rb") as file:
+            new_cake = pickle.load(file)
+        cls.bakery_offer.append(new_cake)
+        return new_cake
+
+    @staticmethod
+    def get_bakery_files(path):
+        return glob.glob(path)
 
 
+vanila_cake = Cake("Vanila Cake","cake","vanilla", ["chocolade","nuts"], "cream", False, "Micu")
+chocolade_muffin = Cake("Chocolade Muffin", "muffin","chocolade", "", "", True, "Micu")
+cake04 = Cake('Cocoa waffle','waffle','cocoa',[],'cocoa', False, "Micu")
+vanila_cake.Text = "100 lat! Micu"
+vanila_cake.save_to_file(r'C:\Users\admin\PycharmProjects\Lekcja Klasy\Save\vanila_cake.bakery')
+cake04.save_to_file(r'C:\Users\admin\PycharmProjects\Lekcja Klasy\Save\cake04.bakery')
+vanila_cake.Text = ""
+cake05 = Cake.read_from_file(r'C:\Users\admin\PycharmProjects\Lekcja Klasy\Save\vanila_cake.bakery')
+
+cake_list = Cake.get_bakery_files(r'C:\Users\admin\PycharmProjects\Lekcja Klasy\Save\*.bakery')
 
 #vanila_cake.show_info()
 #chocolade_muffin.show_info()
@@ -44,11 +86,5 @@ cake04 = Cake('Cocoa waffle','waffle','cocoa',[],'cocoa')
 
 for position in Cake.bakery_offer:
     position.show_info()
+print(cake_list)
 
-
-print(isinstance(cake04,Cake))
-print(type(cake04))
-print(vars(cake04))
-print(vars(Cake))
-print(dir(Cake))
-print(dir(cake04))
